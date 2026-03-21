9297d15318000f5c26b919f75be9c432b006b4be
harishbalaji2006 <harishbalaji868@gmail.com>
2026-03-21 09:37:51 +0530

    Time: 6 ms (43.01%) | Memory: 46.7 MB (8.41%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i:nums){
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
+        for (Map.Entry<Integer,Integer> et:hm.entrySet()) {
+            if (et.getValue()==1) {
+                return et.getKey();
             }
         }
         return 0;
